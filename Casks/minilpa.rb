0c7594dfdd53650c2e5578a5773756860988d75a
lincw <56734434+lincww@users.noreply.github.com>
2024-07-28 01:28:21 +0800

    new: minilpa.rb

---
+++ Casks/minilpa.rb
@@ -0,0 +1,21 @@
+cask "minilpa" do
+    version "1.1.0"
+    sha256 :no_check
+
+    arch arm: "aarch64", intel: "x86_64"
+    url "https://github.com/EsimMoe/MiniLPA/releases/download/v#{version}/MiniLPA-macOS-#{arch}.dmg"
+    name "MiniLPA"
+    desc "Professional LPA UI"
+    homepage "https://github.com/EsimMoe/MiniLPA"
+
+    depends_on macos: ">= catalina"
+
+    app "MiniLPA.app"
+
+    uninstall quit:      "moe.sekiu.MiniLPA",
+              launchctl: "moe.sekiu.MiniLPA"
+
+    zap trash: [
+        "~/.minilpa"
+    ]
+end
